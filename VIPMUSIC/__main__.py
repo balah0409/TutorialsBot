e1beffd46f447a72f9f6c3329be82ee9f14a8401
balah0409 <156516578+balah0409@users.noreply.github.com>
2024-05-25 11:49:50 +0530

    Update __main__.py

--- VIPMUSIC/__main__.py
+++ VIPMUSIC/__main__.py
@@ -46,7 +46,6 @@ async def init():
 
     await VIP.start()
     await VIP.decorators()
-    await restart_bots()
     LOGGER("VIPMUSIC").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗩𝗜𝗣 𝗕𝗢𝗬♨️\n╚═════ஜ۩۞۩ஜ════╝")
     await idle()
 
