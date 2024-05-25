1c354dc0d723b2ce83c4b68647399614c2fb9454
balah0409 <156516578+balah0409@users.noreply.github.com>
2024-05-25 11:29:14 +0530

    Update __main__.py

--- VIPMUSIC/__main__.py
+++ VIPMUSIC/__main__.py
@@ -12,7 +12,6 @@ from VIPMUSIC.plugins import ALL_MODULES
 from VIPMUSIC.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 
-from VIPMUSIC.plugins.tools.clone import restart_bots
 
 
 async def init():
