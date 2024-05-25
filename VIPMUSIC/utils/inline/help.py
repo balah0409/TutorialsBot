6fa2989860a9b18b2fb850a7156ae2e591420eb0
balah0409 <156516578+balah0409@users.noreply.github.com>
2024-05-25 13:48:38 +0530

    Update help.py

--- VIPMUSIC/utils/inline/help.py
+++ VIPMUSIC/utils/inline/help.py
@@ -7,8 +7,8 @@ from VIPMUSIC import app
 
 def first_page(_):
     controll_button = [
-        InlineKeyboardButton(text="Menu", callback_data=f"settingsback_helper"),
-        InlineKeyboardButton(text="Next", callback_data=f"dilXaditi"),
+        InlineKeyboardButton(text="ᴍᴇɴᴜ", callback_data=f"settingsback_helper"),
+        InlineKeyboardButton(text="ɴᴇxᴛ", callback_data=f"dilXaditi"),
     ]
     first_page_menu = InlineKeyboardMarkup(
         [
@@ -41,17 +41,11 @@ def first_page(_):
                 InlineKeyboardButton(
                     text=_["H_B_10"], callback_data="help_callback hb10"
                 ),
-            ],
-            [
-                InlineKeyboardButton(
+                 InlineKeyboardButton(
                     text=_["H_B_8"], callback_data="help_callback hb8"
                 ),
             ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_11"], callback_data="help_callback hb11"
-                )
-            ],
+           
             controll_button,
         ]
     )
@@ -60,7 +54,7 @@ def first_page(_):
 
 def second_page(_):
     controll_button = [
-        InlineKeyboardButton(text="๏ ʙᴀᴄᴋ ๏", callback_data=f"settings_back_helper")
+        InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settings_back_helper")
     ]
     second_page_menu = InlineKeyboardMarkup(
         [
@@ -71,16 +65,18 @@ def second_page(_):
                 InlineKeyboardButton(
                     text=_["H_B_15"], callback_data="help_callback hb15"
                 ),
-               
-            ],
-            [
-                
                 InlineKeyboardButton(
                     text=_["H_B_16"], callback_data="help_callback hb16"
                 ),
+               
+            ],
+            [
                 InlineKeyboardButton(
                     text=_["H_B_17"], callback_data="help_callback hb17"
                 ),
+                 InlineKeyboardButton(
+                    text=_["H_B_11"], callback_data="help_callback hb11"
+                )
             ],
            
             controll_button,
@@ -103,55 +99,55 @@ def help_pannel(_, START: Union[bool, int] = None):
         [
             [
                 InlineKeyboardButton(
-                    text="admin",
+                    text="ᴀᴅᴍɪɴ",
                     callback_data="help_callback hb1",
                 ),
                 InlineKeyboardButton(
-                    text="Auth",
+                    text="ᴀᴜᴛʜ",
                     callback_data="help_callback hb2",
                 ),
                 InlineKeyboardButton(
-                    text="Block",
+                    text="ʙʟᴏᴄᴋ",
                     callback_data="help_callback hb3",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text="Gcast",
+                    text="ɢᴄᴀsᴛ",
                     callback_data="help_callback hb4",
                 ),
                 InlineKeyboardButton(
-                    text="Gban",
+                    text="ɢʙᴀɴ",
                     callback_data="help_callback hb12",
                 ),
                 InlineKeyboardButton(
-                    text="Lyrics",
+                    text="ʟʏʀɪᴄs",
                     callback_data="help_callback hb5",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text="Playlist",
+                    text="ᴘʟᴀʏʟɪsᴛ",
                     callback_data="help_callback hb6",
                 ),
                 InlineKeyboardButton(
-                    text="Voice Chat",
+                    text="ᴠᴏɪᴄᴇ ᴄʜᴀᴛ",
                     callback_data="help_callback hb10",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text="Play",
+                    text="ᴘʟᴀʏ",
                     callback_data="help_callback hb8",
                 ),
                 InlineKeyboardButton(
-                    text="Sudo",
+                    text="sᴜᴅᴏ",
                     callback_data="help_callback hb9",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text="Start",
+                    text="sᴛᴀʀᴛ",
                     callback_data="help_callback hb11",
                 ),
             ],
@@ -180,7 +176,7 @@ def private_help_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="Help",
+                text="ʜᴇʟᴘ",
                 callback_data="settings_back_helper",
             ),
         ],
